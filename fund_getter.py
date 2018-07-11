f1ab4ce361a52043792ac9588589621b27eb87e0
NolanSun <shr1213@live.com>
2018-07-11 22:18:50 +0800

    2018/7/11

--- fund_getter.py
+++ fund_getter.py
@@ -13,17 +13,18 @@ class FundGetter(object):
         while 1:
             baseurl = ('http://fund.eastmoney.com/%s.html' % (str(self.fundcode)))
             r = requests.get(baseurl)
-            # time.sleep(5)  # 避免网速低而加载过慢
+            time.sleep(5)  # 避免网速低而加载过慢
             content = r.text
             soup = BeautifulSoup(content, 'lxml')
             divs = soup.find_all(class_='dataNums')
+            print(divs)
             if divs == []or len(divs) != 3:
                 print('获取失败，记录错误...')
                 print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + 'logging error...')
                 with open('C:\\Users\\sunhaoran\\Documents\\fund_log.txt', 'a', encoding='UTF-8')as f:
                     f.write(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
                     f.write(str(soup))
-                    f.write('============================================================')
+                    f.write('=' * 50)
                 print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + 'logged...')
                 print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + '数据获取失败，五分钟后将重试')
                 time.sleep(300)
@@ -36,8 +37,7 @@ class FundGetter(object):
         extent = float(num[6:10])
         extent = extent / 100
         return worth, extent
-'''
+
 FG = FundGetter('040008')
 a, b = FG.get_price()
 print(a, b)
-'''
